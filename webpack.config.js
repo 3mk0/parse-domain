ead755ae57db792749dca1b509affa7e97ef0f9d
Mike <mk@devshell.com>
2018-08-01 20:13:13 +0300

    Init

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        "parseDomain": "./lib/parseDomain.js",
+        "parseDomain.min": "./lib/parseDomain.js"
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        libraryTarget: 'umd',
+        library: 'ParseDomain'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: path.join(__dirname, 'lib'),
+            exclude: /node_modules/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    "presets": [
+                        ["es2015", {
+                            "modules": false
+                        }]
+                    ],
+                    "plugins": ["babel-plugin-transform-class-properties"]
+                }
+            }]
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.min\.js$/,
+            minimize: false
+        })
+    ]
+};
